3b8badf8f2a6c328c0fc3ea406e9121a71537050
Luc Michalski <michalski.luc@gmail.com>
2016-06-12 17:46:49 +0200

    add missing sugar instructions in resources

---
+++ framework/Resources/sugar.cmake
@@ -0,0 +1,21 @@
+# This file generated automatically by:
+#   generate_sugar_files.py
+# see wiki for more info:
+#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+
+if(DEFINED GPUIMAGE_RESOURCES_SUGAR_CMAKE_)
+  return()
+else()
+  set(GPUIMAGE_RESOURCES_SUGAR_CMAKE_ 1)
+endif()
+
+include(sugar_files)
+
+sugar_files(
+    GPUIMAGE_RESOURCES
+    lookup.png
+    lookup_amatorka.png
+    lookup_miss_etikate.png
+    lookup_soft_elegance_1.png
+    lookup_soft_elegance_2.png
+)
