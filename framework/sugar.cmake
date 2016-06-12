a97cb2fae682f425ec81c1d17cb5bbbc6d43b715
Luc Michalski <michalski.luc@gmail.com>
2016-06-12 11:31:55 +0200

    fix suga missing file for iOS frameworks

---
+++ framework/sugar.cmake
@@ -0,0 +1,15 @@
+# This file generated automatically by:
+#   generate_sugar_files.py
+# see wiki for more info:
+#   https://github.com/ruslo/sugar/wiki/Collecting-sources
+
+if(DEFINED GPUIMAGE_SUGAR_CMAKE_)
+  return()
+else()
+  set(GPUIMAGE_SUGAR_CMAKE_ 1)
+endif()
+
+include(sugar_include)
+
+sugar_include(Resources)
+sugar_include(Source)
